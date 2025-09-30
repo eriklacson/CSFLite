01a8b5c947af6d064a591db295d01dbe9a5e9eae
Erik Lacson <elacson@gmail.com>
2025-09-30 17:00:42 +0800

    feature: nuclei integration: added profile.yaml path + started nuclei2csf main stub

--- tools/nuclei2csf.py
+++ tools/nuclei2csf.py
@@ -1,4 +1,19 @@
 # Command line too for converting Nuclie Scan to CSF Subcategories
 
+import json
+
+
 # Local Helper Module
-from nuclei_helpers import load  # noqa: F401
+from nuclei_helpers import load_profile  # noqa: F401
+from assess import get_paths  # noqa: F401
+
+
+def main():
+    paths = get_paths()
+    profile = load_profile(paths["nuclei_profile"])
+
+    print(json.dumps(profile, indent=4))
+
+
+if __name__ == "__main__":
+    main()

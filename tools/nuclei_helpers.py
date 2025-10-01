6d457b75a9c8465a95ac05c448466b2bb32a7ab0
Erik Lacson <elacson@gmail.com>
2025-10-01 09:37:07 +0800

    feature: nuclei integration - get_profile module stub

--- tools/nuclei_helpers.py
+++ tools/nuclei_helpers.py
@@ -13,4 +13,25 @@ def load_profiles(profile_path: str) -> Dict[str, Any]:
     """Load profiles from a YAML file."""
     with open(profile_path, "r") as file:
         profiles = yaml.safe_load(file)
+
+    """return the profiles dictionary"""
     return profiles
+
+
+def get_profile(profiles: dict, profile: str, default=None, allow_null=False):
+    """
+    Retrieve a specific profile from profiles dict using the profile name,
+    """
+
+    if not isinstance(profiles, dict):
+        raise TypeError("Expected a dictionary as 'data'.")
+
+    if profile not in profiles:
+        return default
+
+    profile = profiles[profile]
+
+    if profile is None and not allow_null:
+        return default
+
+    return profile

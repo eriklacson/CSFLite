8130771aa1432c89a1ad08842e624469eb93fef5
Erik Lacson <elacson@gmail.com>
2026-01-05 15:39:56 +0800

    refactor: added CLI arguments to governance_check.py

--- tools/governance_check.py
+++ tools/governance_check.py
@@ -1,9 +1,10 @@
 """Tool for assessing governance baseline from manual governance checklist."""
 
+# import libraries
+import argparse
 import sys
-
-# import standard libraries
 from pathlib import Path
+from typing import Iterable, Optional
 
 # adjust path for relative imports when executed as script
 if __package__ in (None, ""):
@@ -17,25 +18,59 @@ import tools.global_helpers as global_helpers
 __all__ = [*assess.__all__, "main"]
 
 
-def main():
-    paths = global_helpers.get_paths()
+def parse_args(argv: Optional[Iterable[str]] = None) -> argparse.Namespace:
+    """Parse command line arguments for the Nuclei scanning CLI."""
 
-    # CSF lookup
-    csf_lookup = assess.get_csf_lookup(paths["csf_lookup"])
+    parser = argparse.ArgumentParser(description="CSFLite governance baseline assessment tool")
+    parser.add_argument("--governance_checklist", type=str, help="Governance checklist CSV File")
+    parser.add_argument("--governance_assessment", type=str, help="Governance assessment ouput the CSV")
+    parser.add_argument("--governance_heatmap", type=str, help="Governance assessment heatmap ouput")
 
-    # map governance check to CSF
-    #
-    # get check questionaire result
-    governance_checklist_results = assess.get_governance_checklist_results(paths["governance_checklist"])
+    return parser.parse_args(argv)
 
-    """generate governance assessment"""
 
-    # write governance assessment
-    governance_assessment = assess.generate_governance_assessement(governance_checklist_results, csf_lookup)
-    global_helpers.write_to_csv(governance_assessment, paths["governance_assessment_csv"])
+def validate_arguments(args: argparse.Namespace) -> None:
+    """Validate that both arguments are provided."""
+    if not args.nuclei_raw or not args.csflite_out:
+        raise ValueError(
+            "'--governance_checklist' '--governance_assessment' '--goverance_heatmap' arguments are required."
+        )
 
-    goverance_heatmap = assess.generate_governance_heatmap(governance_assessment)
-    global_helpers.write_to_csv(goverance_heatmap, paths["governance_heatmap_csv"])
+
+def main(argv: Optional[Iterable[str]] = None) -> None:
+    try:
+        args = parse_args(argv)
+
+        # Validate the arguments
+        validate_arguments(args)
+
+        # get reference file paths
+        paths = global_helpers.get_paths()
+
+        # get command line params
+        governance_checklist = args.governance_checklist
+        governance_assessment = args.governance_assessment
+        governance_heatmap = args.governance_heatmap
+
+        # get CSF lookup reference
+        csf_lookup = assess.get_csf_lookup(paths["csf_lookup"])
+
+        # map governance check to CSF
+        #
+        # get check questionaire result
+        governance_checklist_results = assess.get_governance_checklist_results(governance_checklist)
+
+        """generate governance assessment"""
+        # write governance assessment score
+        governance_assessment = assess.generate_governance_assessement(governance_checklist_results, csf_lookup)
+        global_helpers.write_to_csv(governance_assessment, governance_assessment)
+
+        # write governance assessment heatmap
+        governance_heatmap = assess.generate_governance_heatmap(governance_assessment)
+        global_helpers.write_to_csv(governance_heatmap, governance_heatmap)
+
+    except ValueError as ve:
+        print(f"Argument Error: {ve}", file=sys.stderr)
 
 
 if __name__ == "__main__":
